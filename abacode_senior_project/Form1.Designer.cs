0eb3a0ace4e13463a05cce20057e1fb1b1f54f47
smoogiee94 <Carlos.a.marque@gmail.com>
2016-10-18 22:10:30 -0400

    Additional Features
    Added extension checking (prevents non-csv from opening in openVAS)
    Added parsing for openVAS CSV file
    Corrected an issue with openXML SDK not releasing allocated resources
    Forced excel application instance to turn off notices (now automatically
    overwrites a converted file if it already exists)

--- abacode_senior_project/Form1.Designer.cs
+++ abacode_senior_project/Form1.Designer.cs
@@ -120,6 +120,7 @@
             this.startButton.TabIndex = 8;
             this.startButton.Text = "Start";
             this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
             // 
             // Form1
             // 
--- abacode_senior_project/Form1.cs
+++ abacode_senior_project/Form1.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace abacode_senior_project
 {
@@ -41,6 +42,122 @@ namespace abacode_senior_project
             if (browseDialog.ShowDialog() == DialogResult.OK)
             {
                 pathTextBox.Text = browseDialog.FileName;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //-------------------------
+            //openVAS Report Parsing
+            //-------------------------
+            if (openVASRadio.Checked == true)
+            {
+                //----------------------------------
+                // Ensure a path has been specified
+                //----------------------------------
+                if (pathTextBox.Text == "")
+                {
+                    MessageBox.Show("Please enter a path to an openVAS file.");
+                }
+                else
+                {
+                    String path = Directory.GetCurrentDirectory();
+                    var excelApp = new Excel.Application();
+                    excelApp.DisplayAlerts = false;
+                    var excelWorkbooks = excelApp.Workbooks;
+                    
+
+                    //-------------------------
+                    // Check file extension
+                    //-------------------------
+                    if (Path.GetExtension(pathTextBox.Text).Equals(".csv"))
+                    {
+                        //-------------------------------------------------
+                        // This opens the csv file and converts it to xlsx
+                        // Saves it in the specified path in the text box
+                        //-------------------------------------------------
+                        try
+                        {
+                            excelWorkbooks.OpenText(pathTextBox.Text,
+                                    DataType: Excel.XlTextParsingType.xlDelimited,
+                                    TextQualifier: Excel.XlTextQualifier.xlTextQualifierNone,
+                                    ConsecutiveDelimiter: true,
+                                    Semicolon: true);
+
+                            excelWorkbooks[1].SaveAs(pathTextBox.Text + ".xlsx", FileFormat: Microsoft.Office.Interop.Excel.XlFileFormat.xlOpenXMLWorkbook);
+                            excelWorkbooks[1].Close(); //closes first workbook
+
+                            
+                            Excel._Worksheet convertedCSVWorksheet = excelWorkbooks.OpenXML(pathTextBox.Text + ".xlsx").ActiveSheet; //opens another workbook on index 1
+
+
+
+
+                            //--------------------------------------
+                            // File is now opened and ready. We must
+                            // Now create a map and store all values
+                            // for a vulnerability. Vulnerabilities
+                            // are found by Plugin ID value
+                            //--------------------------------------
+                            /*
+                             * Note: Use SortedDictionary with <int, List<String[]>> as the map
+                             * Thus, we can use the Plugin ID and find all vulnerabilities
+                             * of a given vulnerability
+                             *
+                             */
+
+                            Excel.Range usedRange = convertedCSVWorksheet.UsedRange;
+                            for (int i = 0; i < usedRange.Rows.Count; ++i)
+                            {
+                                //---------------------------
+                                // Skip first row of headers
+                                //---------------------------
+                                if (i == 0)
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+//                                    MessageBox.Show(usedRange.Rows.Cells[i + 1, 1].Value.ToString());
+                                }
+                            }
+
+
+
+                            //---------------------------------------------------
+                            // Close worksheets and workbooks. Release Resources
+                            // and close excel
+                            //---------------------------------------------------
+                            System.Runtime.InteropServices.Marshal.ReleaseComObject(convertedCSVWorksheet);
+                            excelWorkbooks[1].Close();
+                            excelWorkbooks.Close();
+                            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelWorkbooks);
+                            excelApp.Quit();
+                            System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+                        }
+                        catch (Exception err)
+                        {
+                            MessageBox.Show("Error encountered: " + err);
+                        }
+                    }
+
+                    else
+                    {
+                        MessageBox.Show("Please select a CSV file.");
+                    }
+
+                    
+                    
+
+                 }
+            }
+
+            //-----------------------
+            // Nessus Report Parsing
+            //-----------------------
+            else
+            {
+
             }
         }
     }
